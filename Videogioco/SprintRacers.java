5b45d520486e6fcbe85ac577f1cb63db4f41b9d7
Daniele Morelli <Daniele Morelli>
2024-03-26 11:00:06 +0100

    inserite immagini + base programma

---
+++ Videogioco/SprintRacers.java
@@ -0,0 +1,18 @@
+package it.edu.iisgubbio.Videogioco;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SprintRacers extends Application{
+
+	@Override
+	public void start(Stage finestra) throws Exception {
+
+		
+		
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
