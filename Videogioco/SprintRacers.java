255929007d9dda52b35ef666def2580a41eedfee
Silvia Pasquini <daniele.morelli@iisgubbio.edu.it>
2024-04-06 16:52:17 +0200

    centrate immagini

--- Videogioco/SprintRacers.java
+++ Videogioco/SprintRacers.java
@@ -14,12 +14,26 @@ import javafx.stage.Stage;
 
 public class SprintRacers extends Application{
 	
-	int i=0;
+	int MacchinaG1, MacchinaG2;
+	
+	Hashtable <Integer,Image> SceltaMacchine =  new Hashtable <Integer,Image>();
+	
+	Image AlfaRomeo = new Image(getClass().getResourceAsStream("AlfaRomeo.png"));
+	Image AlphaTauri = new Image(getClass().getResourceAsStream("AlphaTauri.png"));
+	Image Alpine = new Image(getClass().getResourceAsStream("Alpine.png"));
+	Image AstonMartin = new Image(getClass().getResourceAsStream("AstonMartin.png"));
+	Image Ferrari = new Image(getClass().getResourceAsStream("Ferrari.png"));
+	Image Haas = new Image(getClass().getResourceAsStream("Haas.png"));
+	Image McLaren = new Image(getClass().getResourceAsStream("McLaren.png"));
+	Image Mercedes = new Image(getClass().getResourceAsStream("Mercedes.png"));
+	Image RB = new Image(getClass().getResourceAsStream("RB.png"));
+	
+	ImageView iAlfaRomeo= new ImageView(AlfaRomeo);
 	
 	public void start(Stage finestra) {
 		
 		GridPane griglia= new GridPane();
-		griglia.setHgap(40);
+		griglia.setHgap(20);
 		griglia.setVgap(20);
 		griglia.setId("sfondoSP");
 		griglia.setPadding(new Insets(10, 10, 10 ,10));
@@ -59,23 +73,10 @@ public class SprintRacers extends Application{
 		pComandi.setOnAction(e -> Comandi());
 	}
 	
-	public class Macchina extends Stage {
+	public class MacchinaG1 extends Stage {
+		int i=0;
 		
-		Hashtable <Integer,Image> SceltaMacchine =  new Hashtable <Integer,Image>();
-		
-		Image AlfaRomeo = new Image(getClass().getResourceAsStream("AlfaRomeo.png"));
-		Image AlphaTauri = new Image(getClass().getResourceAsStream("AlphaTauri.png"));
-		Image Alpine = new Image(getClass().getResourceAsStream("Alpine.png"));
-		Image AstonMartin = new Image(getClass().getResourceAsStream("AstonMartin.png"));
-		Image Ferrari = new Image(getClass().getResourceAsStream("Ferrari.png"));
-		Image Haas = new Image(getClass().getResourceAsStream("Haas.png"));
-		Image McLaren = new Image(getClass().getResourceAsStream("McLaren.png"));
-		Image Mercedes = new Image(getClass().getResourceAsStream("Mercedes.png"));
-		Image RB = new Image(getClass().getResourceAsStream("RB.png"));
-		
-		ImageView iAlfaRomeo= new ImageView(AlfaRomeo);
-		
-		public Macchina() {
+		public MacchinaG1() {
 			
 			SceltaMacchine.put(0, AlfaRomeo);
 			SceltaMacchine.put(1, AlphaTauri);
@@ -88,10 +89,9 @@ public class SprintRacers extends Application{
 			SceltaMacchine.put(8, RB);
 			
 			GridPane griglia= new GridPane();
+			
 			griglia.setVgap(10);
 			griglia.setHgap(10);
-			griglia.setId("GP");
-			
 			
 			Scene scene= new Scene(griglia );
 			setTitle("Macchina G1");
@@ -99,39 +99,106 @@ public class SprintRacers extends Application{
 			
 			scene.getStylesheets().add("it/edu/iisgubbio/Videogioco/SprintRacer.css");
 			
-			Label lGiocatore1= new Label("Macchina G1");
-			Label lGiocatore2= new Label("Macchina G2");
+			Label lGiocatore = new Label("Macchina G1");
 			
 			Button pAvanti = new Button(">");
 			Button pIndietro = new Button("<");
+			Button pConferma= new Button("CONFERMA SCELTA");
 			
 			griglia.add(pIndietro, 0, 1);
-			griglia.add(lGiocatore1, 1, 0);
+			griglia.add(lGiocatore, 1, 0);
 			griglia.add(iAlfaRomeo, 1, 1);
 			griglia.add(pAvanti, 2, 1);
+			griglia.add(pConferma, 1, 2);
 			
 			pAvanti.setOnAction(e -> Avanti());
 			pIndietro.setOnAction(e -> Indietro());
-			
+			pConferma.setOnAction(e -> Conferma());
 		}
 		private void Avanti() {
-			
 			i++;
 			
 			if(i > 8) {
 				i=0;
 			}
-			
 			iAlfaRomeo.setImage(SceltaMacchine.get(i));
 		}
 		private void Indietro() {
-			
 			i--;
 			
 			if(i < 0) {
 				i=8;
 			}
 			iAlfaRomeo.setImage(SceltaMacchine.get(i));
+		}
+		private void Conferma() {
+			MacchinaG1 = i; 
+			
+		}
+	}
+	
+	public class MacchinaG2 extends Stage {
+		int i=0;
+		
+		public MacchinaG2() {
+			
+			SceltaMacchine.put(0, AlfaRomeo);
+			SceltaMacchine.put(1, AlphaTauri);
+			SceltaMacchine.put(2, Alpine);
+			SceltaMacchine.put(3, AstonMartin);
+			SceltaMacchine.put(4, Ferrari);
+			SceltaMacchine.put(5, Haas);
+			SceltaMacchine.put(6, McLaren);
+			SceltaMacchine.put(7, Mercedes);
+			SceltaMacchine.put(8, RB);
+			
+			GridPane griglia= new GridPane();
+			
+			griglia.setVgap(10);
+			griglia.setHgap(10);
+			
+			Scene scene= new Scene(griglia );
+			setTitle("Macchina G2");
+			setScene(scene);
+			
+			scene.getStylesheets().add("it/edu/iisgubbio/Videogioco/SprintRacer.css");
+			
+			Label lGiocatore= new Label("Macchina G2");
+			
+			Button pAvanti = new Button(">");
+			Button pIndietro = new Button("<");
+			Button pConferma= new Button("CONFERMA SCELTA");
+			
+			griglia.add(pIndietro, 0, 1);
+			griglia.add(lGiocatore, 1, 0);
+			griglia.add(iAlfaRomeo, 1, 1);
+			griglia.add(pAvanti, 2, 1);
+			griglia.add(pConferma, 1, 2);
+			
+			pAvanti.setOnAction(e -> Avanti());
+			pIndietro.setOnAction(e -> Indietro());
+			pConferma.setOnAction(e -> Conferma());
+		}
+		private void Avanti() {
+			i++;
+			
+			if(i > 8) {
+				i=0;
+			}
+			iAlfaRomeo.setImage(SceltaMacchine.get(i));
+		}
+		private void Indietro() {
+			i--;
+			
+			if(i < 0) {
+				i=8;
+			}
+			iAlfaRomeo.setImage(SceltaMacchine.get(i));
+		}
+		
+		private void Conferma() {
+			MacchinaG2 = i; 
+			
 		}
 	}
 	
@@ -173,8 +240,11 @@ public class SprintRacers extends Application{
 	
 	private void Macchina() {
 		
-		Macchina finestra= new Macchina();
-		finestra.showAndWait();
+		MacchinaG1 finestraG1= new MacchinaG1();
+		finestraG1.showAndWait();
+		
+		MacchinaG2 finestraG2= new MacchinaG2();
+		finestraG2.showAndWait();
 		
 	}
 	
